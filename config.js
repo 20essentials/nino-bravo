6885ca69a6ce8ba0d4366269b2a6501ac8261843
DominicDev <s0l0estudi4r@gmail.com>
2026-01-29 16:01:30 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'NINO BRAVO';
+export const nameSongs = [
+	"UN BESO Y UNA FLOR", /*1*/
+	"LIBRE", /*2*/
+	"NOELIA", /*3*/
+	"AMÉRICA, AMÉRICA", /*4*/
+	"CARTAS AMARILLAS", /*5*/
+	"TÚ CAMBIARÁS", /*6*/
+	"COMO TODOS", /*7*/
+	"VUELVO A ESTAR SIN TI", /*8*/
+	"NO DEBO PENSAR EN TI", /*9*/
+	"NUESTRO HOGAR SERÁ EL MUNDO", /*10*/
+	"ESA SERÁ MI CASA", /*11*/
+	"VOY BUSCANDO", /*12*/
+	"EN LIBERTAD", /*13*/
+	"ES EL VIENTO", /*14*/
+	"MI QUERIDA MAMÁ", /*15*/
+	"AQUEL ATARDECER", /*16*/
+	"QUIÉN ERES TÚ", /*17*/
+	"TE AMARÉ", /*18*/
+	"VUELVE", /*19*/
+	"CANTARÉ", /*20*/
+]
